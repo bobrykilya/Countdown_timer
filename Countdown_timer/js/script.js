a26184d6773b419dd695a7fffec431cbc7af4cfd
bobrikilya <ilya.bobrik.2020@gmail.com>
2022-06-29 21:49:15 +0300

    made the first part of the counting algorithm

--- Countdown_timer/js/script.js
+++ Countdown_timer/js/script.js
@@ -1,2 +1,20 @@
-const currentDate = new Date();
-console.log(currentDate);
+const myCelebration = "4 Sept 2022";
+
+function countdown(){
+  const currentDate = new Date();
+  const myCelebrationDate = new Date(myCelebration);
+  const resultDate = myCelebrationDate - currentDate;
+
+  const resultSeconds = Math.floor(resultDate / 1000);
+  const resultDays = Math.floor(resultSeconds / 60 / 60 / 24);
+  const resultHours = Math.floor(resultSeconds / 60 / 60 );
+  const resultMinutes = Math.floor(resultSeconds / 60);
+ 
+ 
+  console.log(resultDays);
+  console.log(resultHours);
+  console.log(resultMinutes);
+  console.log(resultSeconds);
+}
+
+countdown();
