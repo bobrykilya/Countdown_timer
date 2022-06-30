ba15e0de0fe59f0e3f39f56214b18d8ce5228ce0
bobrikilya <ilya.bobrik.2020@gmail.com>
2022-06-30 14:46:08 +0300

    finished timer algorithm

--- Countdown_timer/js/script.js
+++ Countdown_timer/js/script.js
@@ -1,20 +1,27 @@
-const myCelebration = "4 Sept 2022";
+const myCelebration = "4 Sept 2022"; //12.00
 
 function countdown(){
   const currentDate = new Date();
   const myCelebrationDate = new Date(myCelebration);
   const resultDate = myCelebrationDate - currentDate;
 
-  const resultSeconds = Math.floor(resultDate / 1000);
-  const resultDays = Math.floor(resultSeconds / 60 / 60 / 24);
-  const resultHours = Math.floor(resultSeconds / 60 / 60 );
-  const resultMinutes = Math.floor(resultSeconds / 60);
+  const totalSeconds = Math.floor(resultDate / 1000);
+  const resultDays = Math.floor(totalSeconds / 60 / 60 / 24);
+  const resultHours = Math.floor(totalSeconds / 60 / 60 - resultDays * 24);
+  const resultMinutes = Math.floor(totalSeconds / 60 - resultDays * 24 * 60 - resultHours * 60);
+  const resultSeconds = Math.floor(totalSeconds - resultDays * 24 * 60 * 60 - resultHours * 60 * 60 - resultMinutes * 60);
  
- 
-  console.log(resultDays);
-  console.log(resultHours);
-  console.log(resultMinutes);
-  console.log(resultSeconds);
-}
+  // console.log("Days: " + resultDays);
+  // console.log("Hours: " + resultHours);
+  // console.log("Minutes: " + resultMinutes);
+  // console.log("Seconds: " + resultSeconds);
+  // console.log("---------");
 
-countdown();
+  htmlwriter(resultDays, resultHours, resultMinutes, resultSeconds);
+}; 
+
+function htmlwriter(resultDays, resultHours, resultMinutes, resultSeconds){
+  const dayNumber = document.querySelector('.days p')
+  console.log(dayNumber)
+}
+// setInterval(countdown, 1000);
