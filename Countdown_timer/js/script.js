75c631ae74c4eba0d1bd1aa086444db1f15c5300
bobrikilya <ilya.bobrik.2020@gmail.com>
2022-06-30 15:38:18 +0300

    timer is ready

--- Countdown_timer/js/script.js
+++ Countdown_timer/js/script.js
@@ -1,27 +1,31 @@
 const myCelebration = "4 Sept 2022"; //12.00
 
 function countdown(){
-  const currentDate = new Date();
-  const myCelebrationDate = new Date(myCelebration);
-  const resultDate = myCelebrationDate - currentDate;
+    const currentDate = new Date();
+    const myCelebrationDate = new Date(myCelebration);
+    const resultDate = myCelebrationDate - currentDate;
 
-  const totalSeconds = Math.floor(resultDate / 1000);
-  const resultDays = Math.floor(totalSeconds / 60 / 60 / 24);
-  const resultHours = Math.floor(totalSeconds / 60 / 60 - resultDays * 24);
-  const resultMinutes = Math.floor(totalSeconds / 60 - resultDays * 24 * 60 - resultHours * 60);
-  const resultSeconds = Math.floor(totalSeconds - resultDays * 24 * 60 * 60 - resultHours * 60 * 60 - resultMinutes * 60);
- 
-  // console.log("Days: " + resultDays);
-  // console.log("Hours: " + resultHours);
-  // console.log("Minutes: " + resultMinutes);
-  // console.log("Seconds: " + resultSeconds);
-  // console.log("---------");
+    const totalSeconds = Math.floor(resultDate / 1000);
 
-  htmlwriter(resultDays, resultHours, resultMinutes, resultSeconds);
+    const resultDays = Math.floor(totalSeconds / 60 / 60 / 24);
+    const resultHours = Math.floor(totalSeconds / 60 / 60) % 24;
+    const resultMinutes = Math.floor(totalSeconds / 60) % 60;
+    const resultSeconds = Math.floor(totalSeconds) % 60;
+
+    htmlwriter(resultDays, resultHours, resultMinutes, resultSeconds);
 }; 
 
 function htmlwriter(resultDays, resultHours, resultMinutes, resultSeconds){
-  const dayNumber = document.querySelector('.days p')
-  console.log(dayNumber)
-}
-// setInterval(countdown, 1000);
+    const dayNumber = document.querySelector('.days p');
+    const hoursNumber = document.querySelector('.hours p');
+    const minutesNumber = document.querySelector('.minutes p');
+    const secondsNumber = document.querySelector('.seconds p');
+
+    dayNumber.innerHTML = resultDays;
+    hoursNumber.innerHTML = resultHours;
+    minutesNumber.innerHTML = resultMinutes;
+    secondsNumber.innerHTML = resultSeconds;
+};
+
+setInterval(countdown, 1000);
+// countdown();
