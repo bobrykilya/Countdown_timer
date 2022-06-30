37624e52533eaadf0d5dc36b195729631f5cefbe
bobrikilya <ilya.bobrik.2020@gmail.com>
2022-06-30 17:17:44 +0300

    Fixed tiny bugs and finished input area

--- Countdown_timer/js/script.js
+++ Countdown_timer/js/script.js
@@ -1,5 +1,6 @@
 const myCelebration = "4 Sept 2022"; //12.00
 
+//Countdown timer algorithm
 function countdown(){
     const currentDate = new Date();
     const myCelebrationDate = new Date(myCelebration);
@@ -15,17 +16,27 @@ function countdown(){
     htmlwriter(resultDays, resultHours, resultMinutes, resultSeconds);
 }; 
 
+//Writer of numbers in HTML from timer
 function htmlwriter(resultDays, resultHours, resultMinutes, resultSeconds){
     const dayNumber = document.querySelector('.days p');
     const hoursNumber = document.querySelector('.hours p');
     const minutesNumber = document.querySelector('.minutes p');
     const secondsNumber = document.querySelector('.seconds p');
 
-    dayNumber.innerHTML = resultDays;
-    hoursNumber.innerHTML = resultHours;
-    minutesNumber.innerHTML = resultMinutes;
-    secondsNumber.innerHTML = resultSeconds;
+    dayNumber.innerText = resultDays;
+    hoursNumber.innerText = resultHours;
+    minutesNumber.innerText = resultMinutes;
+    secondsNumber.innerText = resultSeconds;
 };
 
 setInterval(countdown, 1000);
-// countdown();
+
+//Writer of celebration name from text area and cleaning of area
+document.addEventListener('keyup', function(event){
+    const nameOfHoliday = document.querySelector('.nameOfHoliday');
+    if (event.code === 'Enter' & nameOfHoliday.value != "") {
+        const celebName = document.querySelector('h1');
+        celebName.innerText = nameOfHoliday.value;
+        nameOfHoliday.value = "";
+    };
+});
